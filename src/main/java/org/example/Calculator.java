7ad678199ec6ac462468a9c32008786d19009441
Ketaki-Tam <tamketaki@gmail.com>
2024-09-25 17:45:48 +0530

    Adding calculator files

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,64 @@
+package org.example;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculator {
+    public static void main(String[] args) {
+        Services ser = new Services();
+
+        System.out.println("Calculator: ");
+
+        while(true)
+        {
+            System.out.println("--------------------------------------");
+            System.out.println("Select an option:");
+            System.out.println("1) Square root of a decimal number");
+            System.out.println("2) Factorial of an integer");
+            System.out.println("3) Natural logarithm of a real number (positive)");
+            System.out.println("4) Power of a number");
+            System.out.println("5) Exit");
+
+
+            int choice = inputclass.in.nextInt();
+            if(choice == 1){
+                System.out.println("Enter Positive Real Number for Square Root");
+                Double d = inputclass.in.nextDouble();
+                System.out.println(ser.sqrt(d));
+            }
+            else if (choice == 2)
+            {
+                System.out.println("Enter a number to calculate its factorial:");
+                Integer n = inputclass.in.nextInt();
+                System.out.println(ser.factorial(n));
+            }
+            else if (choice == 3)
+            {
+                System.out.println("Enter Positive Real Number for Logarithm");
+                Double d = inputclass.in.nextDouble();
+                System.out.println(ser.logarithm(d));
+            }
+            else if (choice == 4)
+            {
+                System.out.println("Enter Base and Exponent");
+                Double base = inputclass.in.nextDouble();
+                Double exp = inputclass.in.nextDouble();
+                System.out.println(ser.power(base, exp));
+            }
+            else if (choice == 5)
+            {
+                System.out.println("Exiting -------------------");
+                break;
+            }
+            else
+            {
+                System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
+
+class inputclass
+{
+    public static Scanner in = new Scanner(System.in);
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,56 @@
+import org.example.Services;
+import java.util.Optional;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class CalculatorTest {
+
+    private Services services;
+
+    @Before
+    public void setup(){
+        services = new Services();
+    }
+
+
+    @Test
+    public void test_PositiveFactorial() {
+        assertEquals(720.0, services.factorial(6), 0.0001);
+    }
+
+    @Test
+    public void test_NegativeFactorial() {
+        assertEquals(Optional.ofNullable(services.factorial(-720)), Optional.of(-1));
+    }
+
+    @Test
+    public void test_PositivePower() {
+        assertEquals(81.0, services.power(3.0, 4.0), 0.0001);
+    }
+
+    @Test
+    public void test_ExponentZero() {
+        assertEquals(1.0, services.power(2.0, 0.0), 0.0001);
+    }
+
+    @Test
+    public void test_PositiveLog() {
+        assertEquals(Math.log(10), services.logarithm(10.0), 0.0001);
+    }
+
+    @Test
+    public void test_NonPositiveLog() {
+        assertTrue(Double.isNaN(services.logarithm(0.0)));
+    }
+
+    @Test
+    public void test_Positivesqrt() {
+        assertEquals(4.0, services.sqrt(16.0), 0.0001);
+    }
+
+    @Test
+    public void test_Negativesqrt() {
+        assertTrue(Double.isNaN(services.sqrt(-100.0)));
+    }
+}
